2fc9b9ffe6a538b5ab3e2cf460f73d8d421b4474
zhandos <stickysplay@gmail.com>
2021-10-13 04:04:50 +0600

    feat: add home page with promo

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:dtk_store/presenter/home_page.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -7,8 +8,13 @@ void main() {
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      
+    return  MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: SafeArea(
+        child: Scaffold(
+          body: HomePage(),
+        ),
+      ),
     );
   }
 }
