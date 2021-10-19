fde3a356df40d553c5ad4255400fa16d6ac20547
elevencode <archcores@gmail.com>
2021-10-19 10:29:18 +0500

    address page marker

--- lib/presenter/address/address_page.dart
+++ lib/presenter/address/address_page.dart
@@ -1,4 +1,5 @@
 import 'dart:async';
+import 'dart:html';
 
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
@@ -12,19 +13,16 @@ class AddressPage extends StatefulWidget {
 }
 
 class _AddressPageState extends State<AddressPage> {
-  final Completer<GoogleMapController> _mapController = Completer();
-  // Map<MarkerId, Marker> _markers = <MarkerId, Marker>{};
-  final _markers = <Marker>[
-    Marker(
-      draggable: true,
-      markerId: MarkerId("1"),
-      position: LatLng(-12.046374, -77.042793),
-    ),
-  ];
+  // final Completer<GoogleMapController> _mapController = Completer();
+  // final Map<MarkerId, Marker> _markers = <MarkerId, Marker>{};
+
+  // int _markerIdCounter = 0;
+
+  final List<Marker> _markers = [];
 
   final CameraPosition _initialCameraPosition = const CameraPosition(
     target: LatLng(-12.046374, -77.042793),
-    zoom: 14.4746,
+    zoom: 17.0,
   );
 
   @override
@@ -41,11 +39,55 @@ class _AddressPageState extends State<AddressPage> {
           Expanded(
             child: Stack(children: [
               GoogleMap(
-                  markers: Set<Marker>.of(_markers),
-                  mapType: MapType.normal,
-                  initialCameraPosition: _initialCameraPosition,
-                  onMapCreated: _onMapCreated,
-                  onCameraMove: ((_position) => _updatePosition(_position))),
+                // markers: Set<Marker>.of(_markers.values),
+                markers: _markers.toSet(),
+                mapType: MapType.normal,
+                initialCameraPosition: _initialCameraPosition,
+                // onMapCreated: _onMapCreated,
+                onMapCreated: (controller) {
+                  final marker = Marker(
+                    markerId: MarkerId('0'),
+                    position: LatLng(-12.046374, -77.042793),
+                    visible: false,
+                  );
+                  _markers.add(marker);
+                },
+                myLocationEnabled: true,
+                // onCameraMove: ((_position) => _updatePosition(_position)),
+                onCameraMove: (CameraPosition position) {
+                  setState(() {
+                    _markers.first =
+                        _markers.first.copyWith(positionParam: position.target);
+                  });
+                  // if (_markers.isNotEmpty) {
+                  // setState(() {
+                  //     MarkerId markerId = MarkerId(_markerIdVal());
+                  // Marker? marker = _markers[markerId];
+                  // Marker updatedMarker = marker!.copyWith(
+                  //   positionParam: position.target,
+                  // );
+                  //  _markers[markerId] = updatedMarker;
+                  // });
+                  // MarkerId markerId = MarkerId(_markerIdVal());
+                  // Marker? marker = _markers[markerId];
+                  // Marker updatedMarker = marker!.copyWith(
+                  //   positionParam: position.target,
+                  // );
+
+                  // setState(() {
+                  //   _markers[markerId] = updatedMarker;
+                  // });
+                  // }
+                },
+              ),
+              const Align(
+                alignment: Alignment.center,
+                child: Icon(
+                  Icons.person_pin_circle,
+                  size: 40.0,
+                  color: Colors.purple,
+                ),
+              ),
               Positioned(
                 bottom: 20,
                 child: Padding(
@@ -92,34 +134,50 @@ class _AddressPageState extends State<AddressPage> {
     );
   }
 
-  void _updatePosition(CameraPosition _position) {
-    var newMarkerPosition = {
-      "latitude": _position.target.latitude,
-      "longitude": _position.target.longitude
-    };
-    Marker marker = _markers[0];
+  // void _updatePosition(CameraPosition _position) {
+  //   var newMarkerPosition = {
+  //     "latitude": _position.target.latitude,
+  //     "longitude": _position.target.longitude
+  //   };
+  //   Marker marker = _markers[0];
 
-    setState(() {
-      _markers[0] = marker.copyWith(
-          positionParam: LatLng(
-              newMarkerPosition["latitude"]!, newMarkerPosition["longitude"]!));
-    });
-  }
+  //   setState(() {
+  //     _markers[0] = marker.copyWith(
+  //         positionParam: LatLng(
+  //             newMarkerPosition["latitude"]!, newMarkerPosition["longitude"]!));
+  //   });
+  // }
 
-  void _onMapCreated(GoogleMapController controller) async {
-    _mapController.complete(controller);
-    LatLng position = const LatLng(-12.046374, -77.042793);
+  // void _onMapCreated(GoogleMapController controller) async {
+  //   _mapController.complete(controller);
+  //   final LatLng position = LatLng(-12.046374, -77.042793);
 
-    Future.delayed(Duration(seconds: 1), () async {
-      GoogleMapController controller = await _mapController.future;
-      controller.animateCamera(
-        CameraUpdate.newCameraPosition(
-          CameraPosition(
-            target: position,
-            zoom: 17.0,
-          ),
-        ),
-      );
-    });
-  }
+  //   MarkerId markerId = MarkerId(_markerIdVal());
+  //   Marker marker = Marker(
+  //     markerId: markerId,
+  //     position: position,
+  //     draggable: false,
+  //   );
+  //   setState(() {
+  //     _markers[markerId] = marker;
+  //   });
+
+  //   Future.delayed(const Duration(seconds: 1), () async {
+  //     GoogleMapController controller = await _mapController.future;
+  //     controller.animateCamera(
+  //       CameraUpdate.newCameraPosition(
+  //         CameraPosition(
+  //           target: LatLng(position.latitude, position.longitude),
+  //           zoom: 17.0,
+  //         ),
+  //       ),
+  //     );
+  //   });
+  // }
+
+  // String _markerIdVal({bool increment = false}) {
+  //   String val = 'marker_id_$_markerIdCounter';
+  //   if (increment) _markerIdCounter++;
+  //   return val;
+  // }
 }
